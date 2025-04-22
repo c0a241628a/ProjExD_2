bd8b4eb8ed0414f2c4710f941d180f4c6e2dfef0
kaworu <c0a241628a@edu.teu.ac.jp>
2025-04-22 14:04:54 +0900

    ren1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,39 +4,42 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP:(0, -5),
+    pg.K_DOWN:(0, +5),
+    pg.K_LEFT:(-5, 0),
+    pg.K_RIGHT:(5, 0),
+    } #  辞書
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
-    pg.display.set_caption("逃げろ！こうかとん")
-    screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
-    kk_rct = kk_img.get_rect()
-    kk_rct.center = 300, 200
+    pg.display.set_caption("逃げろ！こうかとん")  # タイトル
+    screen = pg.display.set_mode((WIDTH, HEIGHT))  # 6行目
+    bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)  # 0.9倍
+    kk_rct = kk_img.get_rect()  # Rect
+    kk_rct.center = 300, 200  # 初期座標
     clock = pg.time.Clock()
     tmr = 0
-    while True:
+    while True: #ゲームのループ
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])  # 背景の貼り付け
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
-        kk_rct.move_ip(sum_mv)
-        screen.blit(kk_img, kk_rct)
-        pg.display.update()
+        for key,mv in DELTA.items():  #keyにpg,mvにタプルが入る
+            if key_lst[key]:
+                sum_mv[0] += mv[0]  #上下
+                sum_mv[1] += mv[1]  #左右
+                
+        kk_rct.move_ip(sum_mv)  # 合わせて動かす
+        screen.blit(kk_img, kk_rct)  # 移動量
+        pg.display.update()  # 画面の更新
         tmr += 1
-        clock.tick(50)
+        clock.tick(50)  # フレームレート
 
 
 if __name__ == "__main__":
