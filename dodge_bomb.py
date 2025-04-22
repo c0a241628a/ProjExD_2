642876adcd05fdffc3a52cbc76f782033f62d483
kaworu <c0a241628a@edu.teu.ac.jp>
2025-04-22 17:31:32 +0900

    docstring

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -47,6 +47,11 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]: #  pgのRectだと伝える,
 
 
 def gameover(screen: pg.Surface) -> None:
+    """
+    引数：画面
+    戻り値：なし
+    爆弾とこうかとんが当たったら表示
+    """
     bl_img = pg.Surface((1100, 650)) #  背景
     bl_img.set_alpha(100)
     pg.draw.rect(bl_img, (0, 0, 0), (0, 0, 1100, 650))
@@ -72,6 +77,11 @@ def gameover(screen: pg.Surface) -> None:
     
 
 def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    引数：大きさのリストと数字のリスト
+    戻り値：爆弾の大きさ、速度
+    爆弾の大きさ、速度に必要な値を返す
+    """
     bb_imgs = []
     for r in range(1, 11):
         bb_img = pg.Surface((20*r, 20*r), pg.SRCALPHA)
@@ -81,12 +91,22 @@ def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
     return bb_imgs, bb_accs
 
 def get_kk_img(sum_mv: tuple[int, int]) -> pg.Surface:
+    """
+    引数：移動量タプル
+    戻り値：MUKIの番号
+    向きに応じて対応した画像を返す
+    """
     return MUKI.get(sum_mv, MUKI[(0, 0)])  # デフォルトで静止画像を返す
 
 
 
 
 def calc_orientation(org: pg.Rect, dst: pg.Rect,  current_xy: tuple[float, float]) -> tuple[float, float]:
+    """
+    引数：鳥の市、玉の位置、距離
+    戻り値：差と今の差
+    正規化した後のものを返す
+    """
     org_x, org_y = org.center
     dst_x, dst_y = dst.center
     dx = dst_x - org_x
