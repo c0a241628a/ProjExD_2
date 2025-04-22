180fe3c0b10b2129e6e0cd5570da4eee13c26ecf
kaworu <c0a241628a@edu.teu.ac.jp>
2025-04-22 14:23:04 +0900

    ren2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -17,9 +18,18 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")  # タイトル
     screen = pg.display.set_mode((WIDTH, HEIGHT))  # 6行目
     bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像
+    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)  # 0.9倍
     kk_rct = kk_img.get_rect()  # Rect
     kk_rct.center = 300, 200  # 初期座標
+    #  設定だからwhileの外↓
+    bb_img = pg.Surface((20,20)) #  空の四角形
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #  (10,10)で中心、半径10
+    bb_img.set_colorkey((0, 0, 0)) #  背景を透明に
+    bb_rct = bb_img.get_rect()  # Rect
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)  # 初期座標
+    vx, vy = +5, +5 #  速度
+    
     clock = pg.time.Clock()
     tmr = 0
     while True: #ゲームのループ
@@ -34,9 +44,12 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]  #上下
                 sum_mv[1] += mv[1]  #左右
-                
         kk_rct.move_ip(sum_mv)  # 合わせて動かす
-        screen.blit(kk_img, kk_rct)  # 移動量
+        screen.blit(kk_img, kk_rct)  # 移動の描画
+        
+        bb_rct.move_ip(vx, vy)  # 爆弾を動かす
+        screen.blit(bb_img, bb_rct)  # 爆弾の描画
+        
         pg.display.update()  # 画面の更新
         tmr += 1
         clock.tick(50)  # フレームレート
